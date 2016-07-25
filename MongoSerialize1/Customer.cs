2c97400bd191edaf0a95967a15298fc851f4c5c5
DPStrip <dpstrip@gmail.com>
2016-07-24 21:02:00 -0500

    App First time

---
+++ MongoSerialize1/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoSerialize1
+{
+    class Customer
+    {
+        [BsonElement("Id")]
+        public ObjectId Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
